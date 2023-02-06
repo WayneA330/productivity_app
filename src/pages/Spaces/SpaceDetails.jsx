b6249046715764920f56a08fe3fd63f766753cc4
Wayne Celestin <>
2023-02-06 10:19:04 +0400

    added inline edit

--- src/pages/Spaces/SpaceDetails.jsx
+++ src/pages/Spaces/SpaceDetails.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import { Box } from "@mui/material";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import InlineEdit from "../../components/InlineEdit";
 
 const SpaceDetails = () => {
+  const [value, setValue] = useState("");
+
   let params = useParams();
 
-  return <div>{params.id}</div>;
+  return (
+    <Box m={2}>
+      <InlineEdit value={value} setValue={setValue} />
+    </Box>
+  );
 };
 
 export default SpaceDetails;
